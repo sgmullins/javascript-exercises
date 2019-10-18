b139b90f25432f55bce5a607c14eb7241cbc5be3
Steve Mullins <sgmullins@gmail.com>
2019-10-18 10:12:32 -0600

    add code to Odin JS modules 1-3

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num === 0){
+        return '';
+    }else if(num > 0){
+        return str.repeat(num);
+    } else {
+        return 'ERROR';
+    }
 
 }
 
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
-}
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
+   }
 module.exports = reverseString
+
+// or, this is a way to reverse a string without using a method
+
+// const reverseString = function(str) {
+//     let newString = "";
+
+//     for(let i = str.length - 1; i >= 0; i--){
+//         newString += str[i];
+//     }
+//     return newString;
+// }
