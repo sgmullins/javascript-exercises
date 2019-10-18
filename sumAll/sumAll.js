cc50390a7879cc6158e2becee17fff975580f2da
Steve Mullins <sgmullins@gmail.com>
2019-10-18 14:16:30 -0600

    add code through sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
-}
+const sumAll = function(min, max) {
+    let sum = 0;
+    if(min < 0 || max < 0){
+      return 'ERROR';
+    }
+    if(typeof min !== 'number' || typeof max !== 'number'){
+      return 'ERROR';
+    }
+    if(min > max){
+      newMax = min;
+      min = max;
+      max = newMax;
+    }
+    for(let i = min; i <= max; i++){
+      sum += i;
+    }
+    return sum;
+  }
+  
 
 module.exports = sumAll
