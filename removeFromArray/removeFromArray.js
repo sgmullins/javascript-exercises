22957f632b68343c68bc8ef24991ac8900fce058
Steve Mullins <sgmullins@gmail.com>
2019-10-19 12:47:56 -0600

    add code for modules through tempConversion

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, args) {
+    return arr.filter(filterer(arguments)); // Pass arguments
+  }
+  
+  function filterer(args) {
+    return function(value) { // Actual filter function
+      for (var i = 1; i < args.length; i++) {
+        if (value === args[i]) // find
+          return false; // remove
+      }
+      return true; // Otherwise keep
+    };
+  }
 
-}
+//   or 
+
+//   const removeFromArray = (arr, ...args) => arr.filter(i => !args.includes(i));
+
 
 module.exports = removeFromArray
